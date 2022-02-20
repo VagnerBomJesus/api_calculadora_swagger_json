7b199d5ee023fa12d80c237255af748004cc106d
Vagner Bom Jesus <vagneripg@gmail.com>
2022-02-20 20:13:19 +0000

    add heroku

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+
+
+app.use(cors());
 
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger.json');
